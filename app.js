0ec58edaa714fcb2ff095982b2af3df1ce9f4e35
fahadkhan917 <fk16181@gmail.com>
2020-08-08 18:23:21 +0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 function check(){
 
-    
+    var scroce=0;
+    var right=document.getElementById('q1-a1');
+    var wrong1=document.getElementById('q1-a2');
+    var wrong2=document.getElementById('q1-a3');
+    var wrong3=document.getElementById('q1-a4');
+   if(right.checked==true){
+  ++scroce;
+  alert("right answer");
 
+   }
+   else{
+  alert("sorry");
 
+   }
+   alert(scroce);
 }
+
