4462e17bd5d305fa387d8bb64603de991b9b63c8
fahadkhan917 <fk16181@gmail.com>
2020-08-11 15:11:51 +0500

    updated

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-function check(){
+// function check(){
 
-    var scroce=0;
-    var right=document.getElementById('q1-a1');
-    var wrong1=document.getElementById('q1-a2');
-    var wrong2=document.getElementById('q1-a3');
-    var wrong3=document.getElementById('q1-a4');
-   if(right.checked==true){
-  ++scroce;
-  alert("right answer");
+//     var scroce=0;
+//     var right=document.getElementById('q1-a1');
+//     var wrong1=document.getElementById('q1-a2');
+//     var wrong2=document.getElementById('q1-a3');
+//     var wrong3=document.getElementById('q1-a4');
+//    if(right.checked==true){
+//   ++scroce;
+//   alert("right answer");
 
-   }
-   else{
-  alert("sorry");
+//    }
+//    else{
+//   alert("sorry");
 
-   }
-   alert(scroce);
+//    }
+//    alert(scroce);
+// }
+
+
+var questionArr=[
+
+{question:"Which of the following is used to read a HTML page and render it?",
+answer:"Web browser",
+options:[
+    "Web Browser",
+    "Web server",
+    "web matrix",
+    "None of the mentioned"
+]
 }
+
+];
 
